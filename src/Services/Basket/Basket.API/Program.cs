0d677f58b871b8e7b5bdcf92c7e1f05baf2812ba
Adam Tworkowski <adam.tworkowski2012@gmail.com>
2023-04-29 02:29:01 +0200

    init

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,28 @@
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuraion = builder.Configuration;
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = configuraion.GetValue<string>("CacheSettings:ConnectionString");
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

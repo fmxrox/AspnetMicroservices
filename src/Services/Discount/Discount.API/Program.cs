e2fd162ab6315c32d4835637d259653da66d6a51
Adam Tworkowski <adam.tworkowski2012@gmail.com>
2023-05-16 13:38:22 +0200

    Discount API adde with docker-override

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,14 +1,28 @@
+using Discount.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDbContext<DBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DemoDbContext")));
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
 
+    var context = services.GetRequiredService<DBContext>();
+    // Note: if you're having trouble with EF, database schema, etc.,
+    // uncomment the line below to re-create the database upon each run.
+    //context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+    DBInitializer.Initialize(context);
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
